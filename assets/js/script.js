3237e05674b724ef94f65ef9ca7b046580571a55
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-20 19:48:10 -0600

    Add Search by city to button, include error handling, show may as world before search,

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,25 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 0, lng: 0 },
+    zoom: 2,
+  });
+}
+
+function searchCity() {
+  const city = document.getElementById('search-input').value;
+  const geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({ address: city }, function(results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      map.setZoom(12);
+      new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
