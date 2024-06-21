01d4fde1d02b6c1024c52488f513f31c9b9e338a
Hamszn <nicholaushamilton@gmail.com>
2024-06-20 21:25:00 -0500

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+
+const cityName = document.querySelector("#cityName");
+const temp = document.querySelector("#temp");
+const main = document.querySelector("#main");
+const  discription = document.querySelector("#description");
+
+input.onsubmit = (e) => {
+    e.preventDefault();
+    weatherUpdate(city.value);
+    city.value = "";
+};
+  
+weatherUpdate = (city) =>{
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+        "GET",
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1e4163859e4c095643dce57ff89370dd`);
+    xhr.send();
+    xhr.onload= () => {
+       if(xhr.status === 404){
+        alert("City not found");
+       } else {
+        var data = JSON.parse(xhr.response);
+        cityName.innerHTML = data.name;
+        temp.innerHTML = `${Math.round(data.main.temp - 459.67)}°F`;
+        main.innerHTML = data.weather[0].main;
+        discription.innerHTML = data.weather[0].discription;
+       }
+    };    
+};
+
+weatherUpdate("NewYork");
