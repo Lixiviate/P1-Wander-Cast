40b6f51fcd5a1d90153150d5c90964b78f501b09
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-23 21:16:07 -0600

    Add comments to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,38 @@
-const searchInput = document.querySelector('#search-input')
+// Selecting search input from DOM
+const searchInput = document.querySelector('#search-input');
+// Declare variables
 let autoComplete;
 let map;
 let infoWindow;
 let placesService;
 
+// Async function to initialize the map
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
+
+  // Create Info Window to display restaurant info
   infoWindow = new google.maps.InfoWindow();
 
+  // Display map as zoomed out on first load
   map = new Map(document.querySelector("#map"), {
     center: { lat: 0, lng: 0 },
     zoom: 2,
     MapId: '8e1d737c8c3da317',
   });
-  
-  // City auto-complete within search-input
+
+  // autocomplete for search input, restricted to cities only
   autoComplete = new google.maps.places.Autocomplete(searchInput, {
     types: ['(cities)']
   });
 }
 
+// Search for a city and center the map on it
 function searchCity(event) {
   event.preventDefault();
-  const city = document.getElementById('search-input').value;
+  const city = searchInput.value;
   const geocoder = new google.maps.Geocoder();
 
+  // Geocoding the city name to get its location
   geocoder.geocode({ address: city }, function(results, status) {
     if (status === 'OK') {
       map.setCenter(results[0].geometry.location);
@@ -35,9 +43,10 @@ function searchCity(event) {
     }
   });
 
-  return false;
+  return false; // Unknown if needed??
 }
 
+// Search for restaurants near a given location and create markers using the Places API
 function searchRestaurants(location) {
   const request = {
     location: location,
@@ -55,23 +64,27 @@ function searchRestaurants(location) {
   });
 }
 
+// Create a marker on the map
 function createMarker(place) {
   const marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location,
   });
 
+  // Add click event listener to display place details
   google.maps.event.addListener(marker, 'click', function() {
     fetchPlaceDetails(place.place_id, marker);
   });
 }
 
+// Fetch and display details of a restaurant in an InfoWindow
 function fetchPlaceDetails(placeId, marker) {
   const request = {
     placeId: placeId,
     fields: ['name', 'vicinity', 'photos']
   };
 
+  // Get details of the place using PlacesService and adding that content to the info window
   placesService.getDetails(request, function(place, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       const photoUrl = place.photos[0].getUrl();
