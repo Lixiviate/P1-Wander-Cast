6cf3f8770834efce3520adc6cc3b0e44d12a936b
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-23 20:25:08 -0600

    update styles to be color black, add places pins to show restraunts with marker including default photo, and add favorites button to info window.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 const searchInput = document.querySelector('#search-input')
+let autoComplete;
 let map;
+let infoWindow;
+let placesService;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
-  
-  map = new Map(document.getElementById("map"), {
+  infoWindow = new google.maps.InfoWindow();
+
+  map = new Map(document.querySelector("#map"), {
     center: { lat: 0, lng: 0 },
     zoom: 2,
     MapId: '8e1d737c8c3da317',
   });
   
-  // City auto-complete
-  autocomplete = new google.maps.places.Autocomplete(searchInput, {
+  // City auto-complete within search-input
+  autoComplete = new google.maps.places.Autocomplete(searchInput, {
     types: ['(cities)']
   });
-
-
-  
 }
 
 function searchCity(event) {
@@ -28,12 +29,63 @@ function searchCity(event) {
     if (status === 'OK') {
       map.setCenter(results[0].geometry.location);
       map.setZoom(15);
-      new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
-      });
+      searchRestaurants(results[0].geometry.location);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+
+  return false;
+}
+
+function searchRestaurants(location) {
+  const request = {
+    location: location,
+    radius: '1500',
+    type: ['restaurant']
+  };
+
+  placesService = new google.maps.places.PlacesService(map);
+  placesService.nearbySearch(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  });
+}
+
+function createMarker(place) {
+  const placeLoc = place.geometry.location;
+  const marker = new google.maps.Marker({
+    map: map,
+    position: placeLoc
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    fetchPlaceDetails(place.place_id, marker);
+  });
+}
+
+function fetchPlaceDetails(placeId, marker) {
+  const request = {
+    placeId: placeId,
+    fields: ['name', 'vicinity', 'photos']
+  };
+
+  placesService.getDetails(request, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      const photoUrl = place.photos[0].getUrl();
+      const placeName = place.name;
+      const placeVicinity = place.vicinity;
+      const content = `<div style="color: black;">
+        <strong style="color: black;">${placeName}</strong><br>
+        <span style="color: black;">${placeVicinity}</span><br>
+        <img src="${photoUrl}" alt="${placeName}" style="width:100px;height:100px;"><br>
+        <button class="button is-primary is-small" onclick="saveToFavorites('${placeName.replace(/'/g, "\\'")}', '${placeVicinity.replace(/'/g, "\\'")}')">Add to Favorites</button>
+      </div>`;
+      infoWindow.setContent(content);
+      infoWindow.open(map, marker);
     }
   });
 }
