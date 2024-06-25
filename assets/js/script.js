697f7d45aaa6a01e2c46c2c2a5a80021382ef254
Hamszn <nicholaushamilton@gmail.com>
2024-06-24 21:52:37 -0500

    script debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 const input = document.querySelector("#input");
-const city = document.querySelector("#city");
+const city = document.querySelector("#search-input");
 
 const cityName = document.querySelector("#cityName");
 const temp = document.querySelector("#temp");
 const main = document.querySelector("#main");
-const  discription = document.querySelector("#description");
+const description = document.querySelector("#description");
 
 input.onsubmit = (e) => {
     e.preventDefault();
@@ -12,14 +12,14 @@ input.onsubmit = (e) => {
     city.value = "";
 };
   
-weatherUpdate = (city) =>{
+weatherUpdate = (city) => {
     const xhr = new XMLHttpRequest();
     xhr.open(
         "GET",
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1e4163859e4c095643dce57ff89370dd`);
         
     xhr.send();
-    xhr.onload= () => {
+    xhr.onload = () => {
        if(xhr.status === 404){
         alert("City not found");
        } else {
@@ -27,9 +27,11 @@ weatherUpdate = (city) =>{
         cityName.innerHTML = data.name;
         temp.innerHTML = `${Math.round(data.main.temp)}°F`;
         main.innerHTML = data.weather[0].main;
-        discription.innerHTML = data.weather[0].discription;
+        description.innerHTML = data.weather[0].description;
+        image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
        }
     };    
 };
 
+// Call the weatherUpdate function with a default city name
 weatherUpdate("New York");
